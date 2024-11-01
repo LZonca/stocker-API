ad8d1d81c50606563f9faea7156f60d41c2b27f4
LZonca <leo.zonca@business-school.academy>
2024-11-01 20:03:29 +0100

    Test: updated middleware registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->appendToGroup('user-mgroup', \App\Http\Middleware\CheckUserSelf::class);
-        $middleware->appendToGroup('group-mgroup',\App\Http\Middleware\CheckGroupAccess::class);
-        $middleware->appendToGroup('owner-mgroup',\App\Http\Middleware\CheckGroupOwnership::class);
-        $middleware->appendToGroup('stock-mgroup',\App\Http\Middleware\CheckStockAccess::class);
-        $middleware->appendToGroup('set-locale', \App\Http\Middleware\SetLocale::class);
+        $middleware->web([
+            \App\Http\Middleware\web\SetLocale::class,
+        ]);
+        $middleware->api([
+            $middleware->appendToGroup('user-mgroup', \App\Http\Middleware\api\CheckUserSelf::class),
+            $middleware->appendToGroup('group-mgroup', \App\Http\Middleware\api\CheckGroupAccess::class),
+            $middleware->appendToGroup('owner-mgroup', \App\Http\Middleware\api\CheckGroupOwnership::class),
+            $middleware->appendToGroup('stock-mgroup', \App\Http\Middleware\api\CheckStockAccess::class),
+            $middleware->appendToGroup('set-locale', \App\Http\Middleware\api\SetLocale::class)
+        ]);
+
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
